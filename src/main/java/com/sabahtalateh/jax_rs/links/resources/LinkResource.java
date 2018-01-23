da5e67ab43f130e81a136fe800a2dd43323d8cfd
Alexandr Kravtsov <sabahtalateh@gmail.com>
2018-01-24 02:27:18 +0300

    Initial.

---
+++ src/main/java/com/sabahtalateh/jax_rs/links/resources/LinkResource.java
@@ -0,0 +1,40 @@
+package com.sabahtalateh.jax_rs.links.resources;
+
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Path("links")
+public class LinkResource {
+
+    private static final AtomicInteger currentId = new AtomicInteger();
+
+    private static final Map<String, String> links = new ConcurrentHashMap<>();
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("{id}")
+    public Response getUrlById(final @PathParam("id") String id) {
+        if (id == null || id.isEmpty()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        final String url = links.get(id);
+        if (url == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(url).build();
+    }
+
+    @PUT
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response shortUrl(final String url) {
+        final int id = currentId.getAndIncrement();
+        links.put(String.valueOf(id), url);
+        return Response.ok(id).build();
+    }
+}
